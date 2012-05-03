4b62725b6a53bdab016a90da09bb1fab5f58cd7a
Justin Sperry <jasperry@colby.edu>
2012-05-02 22:16:13 -0400

    Finished Particle Tracker Framework
    All code for particle tracker is written. Currently writing code to test.
    Here's nuthin'

--- particle_filter.py
+++ particle_filter.py
@@ -24,6 +24,7 @@ class Particle_Filter(Pipeline.ProcessObject):
 		pipeline.ProcessObject.__init__(self, input, inputCount = 2)
 		
 		self.start_position = pos
+		self.stepsize = stepsize
 		self.n = n
 		self.x = numpy.ones((n,2), int) * pos[0]
 		self.hist = None
@@ -39,22 +40,77 @@ class Particle_Filter(Pipeline.ProcessObject):
 			self.hist = self.makeHist(input, self.start_position, self.n)
 			
 		else:
+		
+			#perturb particles, clip for size, make histograms for each particle
 			self.x += numpy.random.uniform(-self.stepsize, self.stepsize, self.x.shape)
+			self.x = self.x.clip(numpy.zeros(2), array(input.shape)-1).astype(int))
+			new_hist = self.make_histogram(input, x, self.n)
+			
+			#calculate weights (as battacharyya distances)
+			w = self.get_weights(new_hist, self.hist)
+			w /= numpy.sum(w)
+			self.getOutput(0).setData(numpy.sum(self.x.T*w))
 			
 			
+			if 1./sum(w**2) < n/2.:
+				self.x = self.x[self.resample(w),:]
+			
+			
+			
 	
+	def resample(self, weights):
+		n = len(weights)
+		indices = []
+		c = [0.] + sum(weights[:i+1]) for i in range(n)]
+		u0 = numpy.random()
+		for u in [(u0+i)/n for i in range(n)]:
+			while u > C[j]:
+				j+=1
+			indices.append(j-1)
+		return indices	
 	
-	
-	#returns a histogram of intensity values over an nxn patch centered over
-	#the specified position
+	#returns a list of histograms of intensity values over an nxn patch
+	#centered over the specified positions
 	def make_histogram(self, img, pos, n):
-		x,y = pos
-		r = n/2
-		patch = ndimage.map_coordinates(img, [[y-r, y+r],[x-r,x+r]])
-		hist, bin_edges = numpy.histogram(patch, bins = 10) #10 bins
-		return hist
+		hists = []
+		for each in pos:
+			x = each[0]
+			y = each[1]
+			r = n/2
+			patch = ndimage.map_coordinates(img, [[y-r, y+r],[x-r,x+r]])
+			hist, bin_edges = numpy.histogram(patch, bins = 10) #10 bins
+			hists.append(hist)
+		return hists
 	
-	#bhattacharyya distance
-	def bhattacharyya(v1, v2)
+	
+	#returns an array of 1/(the bhattacharyya distance from the original
+	#histogram to the current histogram)
+	def get_weights(self,f, fo):
+		weights = []
+		compare = zip(f,fo)
+		for each in compare:
+			weights.append(1./self.bhattacharyya(each[0],each[1]))
+		
+		return numpy.array(weights)
+		
+	#bhattacharyya distance metric
+	def bhattacharyya(self,v1, v2):
 		return -numpy.log((numpy.sqrt(v1*v2)).sum())
 	
+	
+
+
+def test_particle_filter()
+	import glob
+	import os.path
+	
+	files = glob.glob("path")
+
+
+
+if __name__ == "__main__":
+	test_particle_filter()
+
+
+
+
